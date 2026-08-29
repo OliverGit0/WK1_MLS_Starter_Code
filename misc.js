d6d81eb57115f7fac1f9f4c89619dee05b71c3fa
Oliver Gasior <olivergasior1@gmail.com>
2026-08-29 13:04:07 -0400

    Step 2 Practice Session Commit

--- misc.js
+++ misc.js
@@ -1,18 +1,27 @@
 // Part 1 Exercises (use Copilot inline suggestions)
+let x = 5;
 
 // Exercise 1: Comment-Driven Development
-// Function to calculate the factorial of a number
-
+// Function to calculate the factorial of a number 
+function factorial(n) {
+  if (n <= 1) {
+    return 1;
+  }
+  return n * factorial(n - 1);
+}
 
 // Exercise 2: Array Operations
-const numbers = [1, 2, 3, 4, 5];
+const numList = [1, 2, 3, 4, 5];
 
 // Filter even numbers
-
+const evenNumbers = numList.filter(num => num % 2 === 0);
+const oddNumbers = numList.filter(num => num % 2 !== 0);
 
 // Exercise 3: Function from Signature
 // Type this on a new line and press Enter:
 // function reverseString(str)
+function reverseString(str) {
+  return str.split("").reverse().join("");
+}
 
-
-// Exercise 4: Process User Data 
+// Exercise 4: Process User Data
